30f61dcd2aea708c20de3e9e4ab5fa77788adbf5
Riccardo <c.lin@itsrizzoli.it>
2022-05-23 14:27:05 +0200

    fix: add component-Server

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -2,7 +2,73 @@ package org.example;
 
 import com.google.gson.Gson;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Server {
+    static int portNumber = 1234;
+    static PrintWriter out;
     static Gson gson = new Gson();
+    static String json;
+
+    public static void main(String[] args) {
+
+        System.out.println("Start Server");
+
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.out.println("create socket failed");
+            e.printStackTrace();
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.out.println("Accept failed");
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Accettato");
+        try {
+            out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("Write failed");
+            e.printStackTrace();
+        }
+
+
+        BufferedReader in = null;
+
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+        } catch (IOException e) {
+            System.out.println("reader failed");
+            e.printStackTrace();
+        }
+
+        String s = "";
+        try {
+            while ((s = in.readLine()) != null) {
+
+
+                //out.println(s.toUpperCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
