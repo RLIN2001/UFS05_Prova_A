ba43379876e0d47e074f875fbab84ea75bd4cd0b
Riccardo <c.lin@itsrizzoli.it>
2022-05-23 14:36:32 +0200

    fix: add component client

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,56 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        String hostname="127.0.0.1";
+        int portNumber=1234;
 
-        System.out.println( "Hello World!" );
+        try{
+            Socket socket=new Socket(hostname,portNumber);
+            PrintWriter out =
+                    new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            BufferedReader stdIn = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            String userInput;
+
+
+            while(true)
+            {
+                if((userInput = stdIn.readLine())!=null) {
+
+                    out.println(userInput);
+                }
+
+            }
+
+
+        }
+        catch (UnknownHostException e)
+        {
+            System.err.println("don't know about host "+hostname);
+            System.exit(1);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to "+hostname);
+            System.exit(1);
+        }
+
     }
 }
